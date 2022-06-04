f4f38f3a9e53d948a65c73928bb2aaf947fde23f
FelipeOliveiraSuwawa <felipeoliveira2002pires@gmail.com>
2022-06-04 14:46:49 -0300

    a

---
+++ script.js
@@ -0,0 +1,8 @@
+
+const inputs = document.querySelectorAll('.input')
+
+    const handleFocus = (event) =>{
+        console.log(event.target);
+    }
+    inputs.forEach((input)=> input.addEventListener('focus', handleFocus))
+    
